f5d95f4fe97bb3cb6fb7c5b1132c57c3c3521e12
Knight-XAura <shawn.a.harriman@outlook.com>
2023-12-12 18:41:18 -0500

    Fixing player weirdness while hover and renaming files as well as project organization

--- player/roy.gd
+++ player/roy.gd
@@ -1,27 +1,30 @@
 extends CharacterBody3D
 
 @onready var camera: Camera3D = $Camera3D
-var ray_length: int = 2000
+var ray_length: int = 100
+var ray_query = PhysicsRayQueryParameters3D.new()
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-const MIN_LOOK: int = -90
-const MAX_LOOK: int = 90
-const LOOK_SPEED: int = 10
+
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+	ray_query.set_collision_mask(1)
 
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion or event is InputEventScreenDrag:
 		var mouse_pos = get_viewport().get_mouse_position()
 		var space = get_world_3d().direct_space_state
-		var ray_query = PhysicsRayQueryParameters3D.new()
 		ray_query.from = camera.project_ray_origin(mouse_pos)
 		ray_query.to = ray_query.from + camera.project_ray_normal(mouse_pos) * ray_length
 		var raycast_result = space.intersect_ray(ray_query)
 		look_at(raycast_result["position"])
+		print(raycast_result)
 
 
 func _physics_process(delta: float) -> void:
